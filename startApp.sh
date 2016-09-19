f8bddf94eb3255153ebc0049d38ae94c3ddf5fe0
Sachin Badave <sachin.badave.mail@gmail.com>
2016-09-19 18:21:10 +0530

    Initial Check In
    Application Start Up Script

---
+++ startApp.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cd
+cd releases
+mvn exec:exec -Dexec.executable="java" -Dexec.args="-Xmx200m -cp %classpath:target/sachin.devops-0.0.1-SNAPSHOT.jar org.sachin.devops.mongodb.trials.Trial"
+
